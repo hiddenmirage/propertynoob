a6b27b824bc2f6fdda14cd123b7b07b5000a0856
Lee Kuan Yong <kylee.2014@smu.edu.sg>
2018-01-28 15:52:25 +0800

    hahah
    ahaha

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]  # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"][
                         "id"]  # the recipient's ID, which should be your page's facebook ID
-                    message_text = messaging_event["message"]["object"]  # the message's text
+                    message_text = messaging_event["message"]["attachments"]  # the message's text
                     # **********************************DONT REMOVE**********************************************************************************
 
                     response = wit_response(message_text)
